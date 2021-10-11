f8b91ffd357c00fc7961544a9031028f7b30bcb6
atrigg <92318990+atrigg@users.noreply.github.com>
2021-10-11 12:11:36 -0400

    Test update

--- Notes/src/notes/GitHubTest.java
+++ Notes/src/notes/GitHubTest.java
@@ -4,6 +4,7 @@ public class GitHubTest {
 
 	public static void main(String[] args) {
 		System.out.println("This is a test for my GitHub account.");
+		System.out.println("This is an update to the project.");
 
 	}
 
